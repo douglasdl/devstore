d4c1f13e364b63fecb15d7a004ef0b091e486778
Douglas Dias Leal <douglas_san@hotmail.com>
2025-04-03 00:58:04 +0900

    feat: add products search route

---
+++ src/app/api/products/search/route.ts
@@ -0,0 +1,28 @@
+import type { NextRequest } from 'next/server'
+import { z } from 'zod'
+import data from '../data.json'
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { slug: string } }
+) {
+  await new Promise(resolve => setTimeout(resolve, 1000))
+
+  const { searchParams } = request.nextUrl
+
+  const query = z.string().parse(searchParams.get('q'))
+  const product = data.products.filter(product =>
+    product.title.toLocaleLowerCase().includes(query.toLocaleLowerCase())
+  )
+
+  if (!product) {
+    return Response.json(
+      { message: 'Product not found.' },
+      {
+        status: 400,
+      }
+    )
+  }
+
+  return Response.json(product)
+}
